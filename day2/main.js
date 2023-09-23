2e1796e817878fee17e6855f17dfbca4d22e2e88
Your Name <you@example.com>
2023-09-23 20:59:21 +0530

    lecture

---
+++ day2/main.js
@@ -0,0 +1,64 @@
+console.log("this is js");
+// console.log(6 + 7);
+
+// let firstName = "shubham";
+// let secondName = "choudhary";
+
+// console.log(firstName + secondName);
+// // console.log(secondName);
+
+
+// console.log("full name: firstName ", secondName,);
+
+// console.log(`first name: ${firstName}, second name: ${secondName}`);
+
+
+
+// console.log(Math);
+
+// maethods
+
+// console.log(Math.PI);
+// console.log(Math.E);
+
+// console.log(3 ** 4);
+
+
+// console.log(Math.sqrt(10));
+
+// console.log(parseInt(Math.sqrt(10)));
+
+// console.log(Math.round(3.44444444));
+
+// console.log(Math.floor(3.9999999));
+
+// console.log(Math.ceil(3.111111));
+
+// console.log(Math.abs(-1000));
+
+// console.log(Math.abs(1000));
+
+// console.log(Math.min(-1, 100, 3, 5, -4, 200, 50));
+
+
+// console.log(Math.max(-1, 100, 3, 5, -4, 200, 50));
+
+// console.log(Math.random());
+
+
+console.log(Math.floor((Math.random() * 100) + 1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
