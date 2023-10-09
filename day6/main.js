83ab648798461362067781b8f732f4e2e3f3af06
Your Name <you@example.com>
2023-10-09 15:02:55 +0530

    lecture

---
+++ day6/main.js
@@ -0,0 +1,101 @@
+// function declaration
+
+// function fun() {
+//     console.log("Hello world");
+// }
+
+// function call
+// fun();
+
+// console.log(fun);
+// console.log(Math)
+
+
+// Function parameters
+
+// function fun(x, y) {
+//     console.log(x + y);
+// }
+
+// fun(3, 2);
+
+// default Parameters
+
+// function fun(x, y, z = 5) {
+//     console.log(x + y + z);
+// }
+
+// fun(10, 5);
+
+// function fun(x, y, z = 5) {
+//     console.log(x + y + z);
+// }
+
+// fun(10, 5, 20);
+
+
+// function fun() {
+//     console.log(x);
+// }
+
+// fun();
+
+// var x = 10;
+
+// function fun() {
+//     const b = 10;
+//     console.log(b);
+//     console.log(x);
+// }
+
+// fun();
+
+// console.log(typeof (age))
+
+// function isVote() {
+
+//     let age = parseInt(prompt("what is your age"));
+
+//     if (age < 18) {
+//         console.log("you can't vote ");
+//     } else if (age >= 18 && age < 21) {
+//         console.log("you can give vote only");
+//     } else if (age >= 21 && age < 100) {
+//         console.log("you can vote and drink");
+//     } else {
+//         console.log("invalid age");
+//     }
+// }
+
+// isVote();
+
+// for (; ;) {
+//     console.log("3");
+// }
+
+
+// function getIndex(str, ch, ind) {
+//     let count = 0;
+//     let i = 0;
+//     for (; i < str.length; i++) {
+//         if (str[i] === ch) {
+//             count++
+//         }
+//         if (count === ind) {
+//             break;
+//         }
+//     }
+
+//     console.log(i);
+// }
+
+// console.log(getIndex("abiiooooixzziklmioo", "i", 4));
+
+
+// let i = 0;
+
+// for (; i < 5; i += 4) {
+//     console.log("3");
+// }
+
+// console.log(i); 
---
+++ day7/main.js
@@ -0,0 +1,15 @@
+
+
+console.log(x);
+
+function fun() {
+    console.log(x);
+    console.log(b);
+}
+
+var x = 10;
+
+fun();
+
+var b = 5;
+console.log(b);
